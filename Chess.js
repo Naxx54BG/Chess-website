a9d9ff601f8d1ed36b625642f0409e9aacad7a53
Krasi Tuh <134001026+Naxx54BG@users.noreply.github.com>
2023-05-28 11:48:54 +0300

    Update Chess.js

--- Chess.js
+++ Chess.js
@@ -29,8 +29,7 @@ let lstDrag;
 let winDiv=document.getElementById("WinnerDiv");
 
 for(let i=0;i<8;i++){
-	squares[(i*8)].style.marginLeft=(window.innerWidth-(0.96*window.innerHeight))/2+"px";//lol
-	squares[(i*8)+7].style.marginRight=(window.innerWidth-(0.96*window.innerHeight))/2+"px";//lol2
+	squares[(i*8)].style.marginLeft=(window.innerWidth-(1.4*window.innerHeight))/2+"px";//lol
 }
 
 for(let i=0;i<16;i++){
